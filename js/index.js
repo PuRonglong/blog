e211d0483781a49752aff807177b50b9b66c8988
puronglong <1240429156@qq.com>
2016-12-11 17:55:34 +0800

    no message

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 //图片添加materialCss库样式
 $('img').addClass('materialboxed');
 
-//修复多说样式和materialCss样式冲突
-// $('.ds-sync').label.addClass('delete-before-after');
+var $config = {
+	url                 : '', // 网址，默认使用 window.location.href
+	source              : '', // 来源（QQ空间会用到）, 默认读取head标签：<meta name="site" content="http://overtrue" />
+	title               : '', // 标题，默认读取 document.title 或者 <meta name="title" content="share.js" />
+	description         : '', // 描述, 默认读取head标签：<meta name="description" content="PHP弱类型的实现原理分析" />
+	image               : '', // 图片, 默认取网页中第一个img标签
+	sites               : ['weibo','wechat','qq','qzone', 'douban'], // 启用的站点
+	disabled            : ['google', 'facebook', 'twitter'], // 禁用的站点
+	wechatQrcodeTitle   : "微信扫一扫：分享", // 微信二维码提示文字
+	wechatQrcodeHelper  : '<p>微信里点“发现”，扫一下</p><p>便可分享</p>',
+};
+
+$('.social-share').share($config);
