734ba75d924be4a84ade45adad531fc8f8163a99
puronglong <1240429156@qq.com>
2016-12-11 18:20:52 +0800

    no message

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
 //图片添加materialCss库样式
 $('img').addClass('materialboxed');
 
+// 分享组件配置
 var $config = {
-	url                 : '', // 网址，默认使用 window.location.href
-	source              : '', // 来源（QQ空间会用到）, 默认读取head标签：<meta name="site" content="http://overtrue" />
-	title               : '', // 标题，默认读取 document.title 或者 <meta name="title" content="share.js" />
-	description         : '', // 描述, 默认读取head标签：<meta name="description" content="PHP弱类型的实现原理分析" />
-	image               : '', // 图片, 默认取网页中第一个img标签
-	sites               : ['weibo','wechat','qq','qzone', 'douban'], // 启用的站点
+	// url                 : '', // 网址，默认使用 window.location.href
+	// source              : '', // 来源（QQ空间会用到）, 默认读取head标签：<meta name="site" content="http://overtrue" />
+	// title               : '', // 标题，默认读取 document.title 或者 <meta name="title" content="share.js" />
+	// description         : '', // 描述, 默认读取head标签：<meta name="description" content="PHP弱类型的实现原理分析" />
+	// image               : '', // 图片, 默认取网页中第一个img标签
+	sites               : ['weibo','qq','qzone', 'douban'], // 启用的站点
 	disabled            : ['google', 'facebook', 'twitter'], // 禁用的站点
 	wechatQrcodeTitle   : "微信扫一扫：分享", // 微信二维码提示文字
 	wechatQrcodeHelper  : '<p>微信里点“发现”，扫一下</p><p>便可分享</p>',
