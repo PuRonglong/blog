222beaa9981e8c9bcecb4ed005f889642987b3cd
puronglong <puronglong@foxmail.com>
2017-01-31 15:29:21 +0800

    no message

--- js/index.js
+++ js/index.js
@@ -15,3 +15,24 @@ var $config = {
 };
 
 $('.social-share').share($config);
+
+//图片延迟加载
+var num = document.getElementsByTagName('img').length;
+var img = document.getElementsByTagName("img");
+var n = 0; //存储图片加载到的位置，避免每次都从第一张图片开始遍历
+lazyload(); //页面载入完毕加载可视区域内的图片
+window.onscroll = lazyload;
+function lazyload() { //监听页面滚动事件
+    var seeHeight = document.documentElement.clientHeight; //可见区域高度
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop; //滚动条距离顶部高度
+    for (var i = n; i < num; i++) {
+    	//如果图片距离顶部的高度小于可见区域高度加上滚动的高度
+        if (img[i].offsetTop-300 < seeHeight + scrollTop) {
+            if (img[i].getAttribute("src") == "http://www.puronglong.com/images/default.gif") {
+                img[i].src = img[i].getAttribute("data-src");
+            }
+            n = i + 1;
+        }else{
+        }
+    }
+}
